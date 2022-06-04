876339726c7947162b1ce2afca1b1832d444e5b9
gokhandidin <gokhandidin@gmail.com>
2022-06-05 00:33:11 +0300

    Add BaseViewController

---
+++ AppcentNewsApp/AppcentNewsApp/Scenes/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  AppcentNewsApp
+//
+//  Created by GOKHAN on 5.06.2022.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
